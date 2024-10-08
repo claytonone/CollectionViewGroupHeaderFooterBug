5290fe0d89c68f48f44347be3c83221ce5321ee0
Michael Clayton <michael.clayton@royalmail.com>
2024-10-08 20:31:12 +0100

    Initial commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        List<TestGroup> items = [];
+
+        for (int i = 0; i < 4; i++)
+        {
+            var group = new TestGroup()
+            {
+                Title = $"Group {i + 1}"
+            };
+
+            for (int j = 0; j < 4; j++)
+            {
+                group.Add(new TestItem() { Value = $"Sub item {j + 1}" });
+            }
+
+            items.Add(group);
+        }
+
+        ItemsList.ItemsSource = items;
+    }
+}
---
+++ MauiApp1/TestGroup.cs
@@ -0,0 +1,11 @@
+namespace MauiApp1;
+
+public class TestGroup : List<TestItem>
+{
+    public string Title { get; set; }
+}
+
+public class TestItem
+{
+    public string Value { get; set; }
+}
